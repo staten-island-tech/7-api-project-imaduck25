9a742d1a9eacc900ea435559ebc83dd83bad4742
imaduck25 <fatimaa158@nycstudents.net>
2025-12-09 06:46:10 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 import requests
-
-def getPoke(poke):
-    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
+def cp(color):
+    response = requests.get(f"http://colormind.io/api-access/{color.lower()}")
     if response.status_code != 200:
         print("Error fetching data!")
         return None
-    
     data = response.json()
-    return {
-        "name": data["name"],
-        "height": data["height"],
-        "weight": data["weight"],
-        "types": [t["type"]["name"] for t in data["types"]]
-    }
-
-pokemon = getPoke("Bulbasaur")
-print(pokemon)
+    return data
+colorpalette = cp("Bulbasaur")
+print(colorpalette)
