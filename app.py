823eeb26c660900f648883e19d32513ca297ff2d
imaduck25 <fatimaa158@nycstudents.net>
2025-12-10 06:53:25 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import requests
+""" import requests
 import json
 
 def cs():
@@ -14,4 +14,19 @@ def cs():
     return data
 
 palette = cs()
+print(palette) """
+
+
+import requests
+
+def cp():
+    response = requests.get(f"http://colormind.io/api-access/")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    
+    data = response.json()
+    return data
+
+palette = cp()
 print(palette)
