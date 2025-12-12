a1adc2264e339e4f6cafc973483fc306905343a7
imaduck25 <fatimaa158@nycstudents.net>
2025-12-12 06:56:37 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,22 +7,8 @@ def cs():
     print(x)
 cs() """
 
-
 import tkinter as tk
-window = tk.Tk()
-window.title("Label Example")
-# Create a label
-label = tk.Label(
-window,
-    text="Hello, Tkinter!",
 
-    font=("Comic Sans MS", 20, "bold"),
-    fg="white",
-    bg="purple",
-    padx=20,
-    pady=10,
-    relief="ridge"
-)
-# Place it in the window
-label.pack(pady=20)
-window.mainloop()
+window = tk.Tk()
+window.title("Image Example")
+window.geometry("400x400")
