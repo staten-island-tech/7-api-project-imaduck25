446d3838ea1075a4eb19bc80fc5d14795a3a90cd
imaduck25 <fatimaa158@nycstudents.net>
2025-12-15 06:52:11 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-""" import requests
+import requests
 
 def cs():
     data = {"model": "default"}
     response = requests.post("http://colormind.io/api/", json=data)
     x = response.json()
     print(x)
-cs() """
+cs()
 
-import tkinter as tk
-
-window = tk.Tk()
-window.title("Image Example")
-window.geometry("400x400")
