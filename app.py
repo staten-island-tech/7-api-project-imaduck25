93a072f1c91f352ab518c12a1cbbf976aebafbb8
imaduck25 <fatimaa158@nycstudents.net>
2025-12-17 06:52:28 -0800

    Update app.py

--- app.py
+++ app.py
@@ -18,10 +18,12 @@ prompt = tk.Label(root, text="Which color belongs?",
 font=("Serif", 20))
 prompt.grid(row=0, column=2, columnspan=6, pady=10) 
 
-tk.Button(root, text="Wide Button").grid(
-    row=1,
-    column=0,
-    columnspan=2
-)
+frame = tk.Frame(root)
+frame.grid(row=0, column=0)
+
+for i in range(4):
+    root.columnconfigure(i, weight=1)
+    tk.Button(root, text=f"B{i}").grid(row=4, column=i, columnspan=2, sticky="ew")
+
 
 root.mainloop()
