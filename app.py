d934b93db7fe35dd80aad0cbfe689a639e93b73e
imaduck25 <fatimaa158@nycstudents.net>
2025-12-17 06:58:32 -0800

    Update app.py

--- app.py
+++ app.py
@@ -11,18 +11,23 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("Shade Seeker")
-root.geometry("600x500")
+root.geometry("700x500")
 root.resizable(False, False)
 
-prompt = tk.Label(root, text="Which color belongs?",
+prompt = tk.Label(root, text="                      Which color belongs?",
 font=("Serif", 20))
 prompt.grid(row=0, column=2, columnspan=6, pady=10) 
 
 frame = tk.Frame(root)
 frame.grid(row=0, column=0)
 
-for i in range(4):
-    tk.Button(root, text=f"B{i}").grid(row=4, column=i, columnspan=2, sticky="ew")
+def say_hello():
+    print("Hello!")
 
+def say_goodbye():
+    print("Goodbye!")
+
+tk.Button(root, text="Hello", command=say_hello).grid(row=4, column=2, columnspan=3,pady=10)
+tk.Button(root, text="Goodbye", command=say_goodbye).grid(row=5, column=1, columnspan=3, pady=10)
 
 root.mainloop()
