61df57b0098dec73abda97f03c37f9f5ab40b531
imaduck25 <fatimaa158@nycstudents.net>
2025-12-25 16:18:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ root.resizable(False, False)
 root.configure(bg= "#D79179")
 
 prompt = tk.Label(root, text="Which color belongs?",
-font=("Courier New", 20), bg="#D79179", fg="#594654")
+font=("Courier New", 20), 
+bg="#D79179", 
+fg="#594654")
 prompt.grid(row=0, column=2, padx=10, pady=10) 
 
 button_frame = tk.Frame(root, bg="#D79179")
 button_frame.grid(row=2,column=2, pady=20)
-
-buttons= []
 
 def check(answer):
     result_label = prompt
@@ -31,7 +31,6 @@ def check(answer):
         result_label.config(text="Wrong!")
     for btn in buttons:
         btn.config(state=tk.DISABLED)
-        button_frame.destroy()
 
 correct_answer = "2"
 
@@ -40,18 +39,25 @@ def next_round():
     button_frame.destroy()
     button_frame = tk.Frame(root, bg="#D79179")
     button_frame.grid(row=4, column=0, columnspan=4, rowspan = 4, pady=20, padx= 50)
+    buttons = []
     for i in range(4):
             btn = tk.Button(
             button_frame,
             text=i,
             width=10,
+            fg="#A05F62",
+            font=("Courier New", 10, "bold"),
             command=lambda a=i: check(a)
             )
             btn.grid(row=0, column=i, padx=10)
             buttons.append(btn)
 next_round()
 
-next_btn = tk.Button(root, text="Next Round", command=next_round, bg = "#A05F62", fg= "#FAF9FA")
+next_btn = tk.Button(root, 
+text="Next Round", 
+command=next_round, 
+bg = "#859A9D", 
+fg= "#FAF9FA")
 next_btn.grid(row=8, column=0, columnspan=4, rowspan = 4, pady=20)
 
 root.mainloop()
